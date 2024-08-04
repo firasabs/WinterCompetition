04887202a818dfc11168c990367be5279b7ef074
firasabs <firasabosada@gmail.com>
2024-08-04 15:12:46 +0300

    editing on the functions that add competitors

--- src/GUI/CompetitionGUI.java
+++ src/GUI/CompetitionGUI.java
@@ -1,28 +1,25 @@
 package GUI;
+
 import Observe.Observable;
 import Observe.Observer;
-import game.Competition.Competition;
-import game.Competition.Competitor;
-import game.Competition.WinterCompetition;
-import game.entities.sportsman.WinterSportsman;
+import game.entities.MobileEntity;
+import utilities.Point;
 
-import java.util.List;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
-
-public class CompetitionGUI implements Observer {
+public class CompetitionGUI {
     protected static String IMAGE_PATH = "/Users/firasabusada/Desktop/SW/שנה ב סמ ב/תכנות מונחה עצמים מתקדם/HwJava/HW2/icons/";
-    protected static JPanel centerPanel;
+    protected static JLayeredPane centerPanel;
     private static JLabel backgroundLabel;
     static JPanel rightPanel;
     private static int width = 1000;
-    private static boolean gameStarted = false;
-    private static boolean gamefinshed = true;
-    private Competition currcompetition;
-    private static Timer timer;
     private static List<CompetitorLabel> competitorLabels = new ArrayList<>();
+    private Timer timer;
+    private static boolean gameStarted = false;
+    private static boolean gameFinished = true;
 
     public CompetitionGUI() {
         JFrame frame = new JFrame("Competition");
@@ -37,12 +34,12 @@ public class CompetitionGUI implements Observer {
         BuildArenaPanel buildArenaPanel = new BuildArenaPanel();
         CreateCompetitionPanel createCompetitionPanel = new CreateCompetitionPanel();
         AddCompetitorPanel addCompetitorPanel = new AddCompetitorPanel();
-        /**
-         * adding Observer
-         */
+
+        // Adding Observer
         buildArenaPanel.addObserver(createCompetitionPanel);
         createCompetitionPanel.addObserver(addCompetitorPanel);
-        // adding panels to right panel
+
+        // Adding panels to right panel
         rightPanel.add(buildArenaPanel.getPanel());
         rightPanel.add(createCompetitionPanel.getPanel());
         rightPanel.add(addCompetitorPanel.getPanel());
@@ -53,11 +50,15 @@ public class CompetitionGUI implements Observer {
         frame.add(scrollPane, BorderLayout.EAST);
 
         // Center Panel
-        centerPanel = new JPanel();
-        centerPanel.setLayout(new BorderLayout());
+        centerPanel = new JLayeredPane();
+        centerPanel.setLayout(null); // Use null layout to control positions
+        centerPanel.setPreferredSize(new Dimension(getWidth(), BuildArenaPanel.getLength()));
+
+        // Background Label
         backgroundLabel = new JLabel();
-        backgroundLabel.setBackground(Color.LIGHT_GRAY);
-        centerPanel.add(backgroundLabel);
+        backgroundLabel.setBounds(0, 0, getWidth(), BuildArenaPanel.getLength());
+        centerPanel.add(backgroundLabel, JLayeredPane.DEFAULT_LAYER);
+
         frame.add(centerPanel, BorderLayout.CENTER);
         frame.setVisible(true);
     }
@@ -69,70 +70,75 @@ public class CompetitionGUI implements Observer {
     public static int getWidth() {
         return width;
     }
+
     public static void setBackgroundImage(ImageIcon icon) {
-        JLabel backgroundLabel = new JLabel(icon);
-        centerPanel.add(backgroundLabel);
+        Image resizedImg = icon.getImage().getScaledInstance(centerPanel.getWidth(), centerPanel.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImg);
+        backgroundLabel.setIcon(resizedIcon);
+        backgroundLabel.setBounds(0, 0, centerPanel.getWidth(), centerPanel.getHeight());
         centerPanel.revalidate();
         centerPanel.repaint();
     }
+
+    public static void addCompetitorIcon(MobileEntity competitor, ImageIcon icon) {
+        Image resizedImage = icon.getImage().getScaledInstance(65,65,Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+        JLabel competitorLabel = new JLabel(resizedIcon);
+        Point initialPosition = competitor.getLocation();
+        competitorLabel.setBounds((int) initialPosition.getX(), (int) initialPosition.getY(), 65, 65);
+        centerPanel.add(competitorLabel, JLayeredPane.PALETTE_LAYER);
+        centerPanel.setLayer(competitorLabel, JLayeredPane.PALETTE_LAYER);
+        competitorLabels.add(new CompetitorLabel(resizedIcon, competitorLabel, competitor));
+        centerPanel.revalidate();
+        centerPanel.repaint();
+    }
+
     public static boolean isGameStarted() {
         return gameStarted;
     }
-    public static void addCompetitorIcon(Competitor competitor) {
-        ImageIcon icon = competitor.getIcon();
-        Point initialPosition = new Point((int) competitor.getLocation().getX(), (int) competitor.getLocation().getY());
-        JLabel competitorLabel = new JLabel(icon);
-        competitorLabel.setBounds((int) initialPosition.getX(), (int) initialPosition.getY(), 65, 65);
-        centerPanel.add(competitorLabel);
-        competitorLabels.add(new CompetitorLabel(icon, competitorLabel, initialPosition));
-        centerPanel.revalidate();
-        centerPanel.repaint();
-    }
 
-
-    public static void main(String[] args) {
-        CompetitionGUI gui = new CompetitionGUI();
-    }
-
-    @Override
-    public void update(Observable obs, Object arg) {
-        if(obs instanceof Competition) {
-            currcompetition = (Competition) obs;
-        }
-    }
-    private void updateCompetitorPositions() {
-        for (CompetitorLabel competitorLabel : competitorLabels) {
-            Point location = competitorLabel.getInitialPosition();
-            competitorLabel.getLabel().setBounds((int) location.getX(), (int) location.getY(), 65, 65);
-        }
-        centerPanel.repaint();
-    }
     public void startUpdating() {
         timer = new Timer(30, e -> updateCompetitorPositions());
         timer.start();
     }
-    private static class CompetitorLabel {
+
+    private void updateCompetitorPositions() {
+        for (CompetitorLabel competitorLabel : competitorLabels) {
+            Point location = competitorLabel.getMobileEntity().getLocation();
+            competitorLabel.getLabel().setBounds((int) location.getX(), (int) location.getY(), 65, 65);
+        }
+        centerPanel.repaint();
+    }
+
+    public static void main(String[] args) {
+        CompetitionGUI gui = new CompetitionGUI();
+        gui.startUpdating();
+    }
+
+    private static class CompetitorLabel implements Observer {
         private final ImageIcon icon;
         private final JLabel label;
-        private Point initialPosition;
+        private  MobileEntity mobileEntity;
 
-        public CompetitorLabel(ImageIcon icon, JLabel label, Point initialPosition) {
+        public CompetitorLabel(ImageIcon icon, JLabel label, MobileEntity mobileEntity) {
             this.icon = icon;
             this.label = label;
-            this.initialPosition = initialPosition;
-        }
-
-        public ImageIcon getIcon() {
-            return icon;
+            this.mobileEntity = mobileEntity;
         }
 
         public JLabel getLabel() {
             return label;
         }
 
-        public Point getInitialPosition() {
-            return initialPosition;
+        public MobileEntity getMobileEntity() {
+            return mobileEntity;
+        }
+
+        @Override
+        public void update(Observable obs, Object arg) {
+            if (obs instanceof MobileEntity){
+                this.mobileEntity = (MobileEntity)arg;
+            }
         }
     }
 }
-
